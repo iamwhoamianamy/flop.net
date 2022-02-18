61af423c06209560d7017b07a9ba4e3e829b1ea2
murkykiska <catmasya97@gmail.com>
2022-02-18 12:28:32 +0700

    Добавлен NuGet пакет Fluent.Ribbon. Загружен прототип интерфейса основного окна. Добавлена иконка приложения.
    Revert "Добавлен NuGet пакет Fluent.Ribbon. Загружен прототип интерфейса основного окна. Добавлена иконка приложения."
    
    This reverts commit 0e7f15863383eede8cc947d15b892d515fe9a6cd.
    
    Revert "Revert "Добавлен NuGet пакет Fluent.Ribbon. Загружен прототип интерфейса основного окна. Добавлена иконка приложения.""
    
    This reverts commit 128fb690f0a7f5171f8cc578038fda6bc1f08faf.

--- flop.net/View/MainWindow.xaml.cs
+++ flop.net/View/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Fluent;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -18,7 +19,7 @@ namespace flop.net
    /// <summary>
    /// Логика взаимодействия для MainWindow.xaml
    /// </summary>
-   public partial class MainWindow : Window
+   public partial class MainWindow : RibbonWindow
    {
       public MainWindow()
       {
