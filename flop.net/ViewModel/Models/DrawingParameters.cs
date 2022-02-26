449ed4a78bc30d89f61e104a1c135d381895b349
ilusha <Ilya_Khechnev@outlook.com>
2022-02-26 14:39:22 +0700

    Fix 4.3 Drawing Parameters (#35)
    * Test issue
    
    * Delete
    
    * Delete #2
    
    * Добавил шаблоны класса фигуры и ее параметров
    
    * Added check assembly and publication application
    
    * Delete build.yml
    
    * Добавил свойство Points типа PointsCollection и реализовал у Figure INotifyPropertyChanged
    
    * Рспределил файлы по папкам, заменил у Figure тип IGeometric на реализованный во View
    
    * Исрпавил Points - свойство
    
    * Delete test.txt
    
    * Переделан пункт 4.3
    
    Co-authored-by: Artem <strongmouse5088@gmail.com>
    Co-authored-by: Strong0-0Mouse <64701982+Strong0-0Mouse@users.noreply.github.com>
    Co-authored-by: Arthur Efremov <anon.yu@mail.ru>
    Co-authored-by: sergeypobwer <sergeypobwer@yandex.ru>

--- flop.net/ViewModel/Models/DrawingParameters.cs
+++ flop.net/ViewModel/Models/DrawingParameters.cs
@@ -1,13 +1,17 @@
-﻿using System.Windows.Media;
+﻿using System.Collections.Generic;
+using System.Windows.Media;
 using flop.net.ViewModel.Enums;
 
 namespace flop.net.ViewModel.Models
 {
     public abstract class DrawingParameters
     {
-        public TypesFill TypeFill { get; set; }
-        public Colors ColorFill { get; set; }
-        public TypesStroke TypeStroke { get; set; }
-        public Colors ColorStroke { get; set; }
+        public Brush Fill { get; set; }
+        public Brush Stroke { get; set; }
+        public int StrokeThickness { get; set; }
+        public PenLineCap PenLineCap { get; set; }
+        public List<double> StrokeDashArray { get; set; }
+        public double Opacity { get; set; }
+        public int ZIndex { get; set; }
     }
 }
