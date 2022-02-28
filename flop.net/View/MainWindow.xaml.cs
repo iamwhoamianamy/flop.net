d4f37f4c389a00e0608567ef3f4145ee7c802eac
murkykiska <catmasya97@gmail.com>
2022-03-01 01:03:32 +0700

    Изменены аргументы метода DrawPolygon на корректные

--- flop.net/View/MainWindow.xaml.cs
+++ flop.net/View/MainWindow.xaml.cs
@@ -80,7 +80,7 @@ namespace flop.net
             Graphic.CleanCanvas();
             foreach (var x in MainWindowVM.Figures)
             {
-                Graphic.DrawPolygon(x.Geometric.Points);
+                Graphic.DrawPolygon(x.Geometric.Points, x.DrawingParameters);
             }
         }
     }
