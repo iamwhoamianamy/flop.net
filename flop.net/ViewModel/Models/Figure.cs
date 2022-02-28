875c93726c763226c5927972ba775ca73986f938
Ilya Khechnev <Ilya_Khechnev@outlook.com>
2022-02-28 23:15:32 +0700

    Refactor class Figure

--- flop.net/ViewModel/Models/Figure.cs
+++ flop.net/ViewModel/Models/Figure.cs
@@ -1,40 +1,39 @@
-﻿using System.ComponentModel;
+﻿using System;
+using System.ComponentModel;
 using System.Runtime.CompilerServices;
 using System.Windows.Media;
 using flop.net.Model;
 using System.Windows;
-using System.Diagnostics;
+
 namespace flop.net.ViewModel.Models
 {
     public class Figure : INotifyPropertyChanged
     {
-        public event PropertyChangedEventHandler PropertyChanged;
-        
         public enum FigureAction { MOVE, ROTATE, SCALE };
 
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        
         public PointCollection Points { get; set; }
+
+        private static IGeometric emptyGeometric = new Polygon(new PointCollection {new(-1, -1), new(-1, -1)}, true);
+        private IGeometric saveGeometric;
         private IGeometric geometric;
         public IGeometric Geometric 
         {
-            get
-            {
-                return geometric;
-            }
+            get => geometric;
             set
             {
                 geometric = value;
                 OnPropertyChanged();
             }
         }
+
         public DrawingParameters DrawingParameters { get; set; }
 
-        public Figure()
-        {
-        }
-        static Figure()
-        {
-            EmptyGeometric = new Polygon(new PointCollection(){ new Point(-1, -1), new Point(-1, -1) }, true);
-        }
         public Figure(IGeometric geometric, DrawingParameters drawingParameters, PointCollection points)
         {
             Points = points;
@@ -42,44 +41,39 @@ namespace flop.net.ViewModel.Models
             DrawingParameters = drawingParameters;
         }
 
-        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = "")
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-        }
-
-        private static IGeometric EmptyGeometric;
-        private IGeometric SaveGeometric;
-
         public void CreateFigure()
         {
-            Geometric = SaveGeometric;
+            Geometric = saveGeometric;
         }
 
         public void DeleteFigure()
         {
-            SaveGeometric = Geometric;
-            Geometric = EmptyGeometric;
+            saveGeometric = Geometric;
+            Geometric = emptyGeometric;
         }
 
         public void ModifyFigure(FigureAction action, object parameter)
         {
-            if (action == FigureAction.MOVE)
+            switch (action)
             {
-                Vector vector = (Vector)parameter;
-                if (vector != null)
+                case FigureAction.MOVE:
+                {
+                    var vector = (Vector)parameter;
                     Geometric.Move(vector);
-            }
-            if (action == FigureAction.ROTATE)
-            {
-                double angle;
-                double.TryParse(parameter.ToString(), out angle);
-                Geometric.Rotate(angle);
-            }
-            if (action == FigureAction.SCALE)
-            {
-                Point p = (Point)parameter;
-                if (p != null)
+                    break;
+                }
+                case FigureAction.ROTATE:
+                {
+                    var angle = Convert.ToSingle(parameter);
+                    Geometric.Rotate(angle);
+                    break;
+                }
+                case FigureAction.SCALE:
+                {
+                    var p = (Point)parameter;
                     Geometric.Scale(p);
+                    break;
+                }
             }
         }
     }
