61be43cd4bfdbd35accada2da7b0da06a5995c76
sergeypobwer <52440985+sergeypobwer@users.noreply.github.com>
2022-02-28 20:11:14 +0700

    Первый функционал объектного режима (#43)
    * Тестовая ветка для проверки паттерна MVVM и команд в частности. Не изменять.
    
    * На кнопку "Прямоугольник" теперь генерируется соответствующая фигура, канва обновяется автоматически. При кждом нажатии новая фигура в рандомном (пока что) месте. Реализован механизм Undo/Redo. Команде GUI нужно нарисовать это кнопки на пользовательском интерфейсе и за'bind'ится к соответствующим свойствам. Обновлены свойства класса Figure - они теперь public. Сам класс фигур расширился тремя методами: создания, удаления, модификации.
    
    * Реализован весь функционал "объектный режим фигуры": переместить фигуру, масштабировать фигуру, повернуть фигуру, удалить фигуру с полотна. Параметры для изменения фигуры выбираются рандомно. В будущем нужно исправить.
    
    Co-authored-by: murkykiska <catmasya97@gmail.com>

--- flop.net/ViewModel/MainWindowVM.cs
+++ flop.net/ViewModel/MainWindowVM.cs
@@ -1,11 +1,177 @@
-﻿using System.ComponentModel;
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Collections.Generic;
 using System.Runtime.CompilerServices;
+using System.Windows;
 using flop.net.Annotations;
+using flop.net.Model;
+using flop.net.View;
+using System.Windows.Input;
+using flop.net.ViewModel.Models;
+using System.Diagnostics;
 
 namespace flop.net.ViewModel;
 
+public class UserCommands
+{
+    private Action<object> execute;
+    private Action<object> unexecute;
+    public UserCommands(Action<object> execute, Action<object> unexecute)
+    {
+        this.execute = execute;
+        this.unexecute = unexecute;
+    }
+
+    public RelayCommand Execute
+    {
+       get => new RelayCommand(execute);
+    }
+
+    public RelayCommand UnExecute
+    {
+        get => new RelayCommand(unexecute);
+    }
+}
+
 public class MainWindowVM : INotifyPropertyChanged
 {
+    public ObservableCollection<Figure> Figures { get; set; }
+    public Stack<UserCommands> undoStack;
+    public Stack<UserCommands> redoStack;
+
+    private void undoFunc()
+    {
+        if (undoStack.Count > 0)
+        {
+            var undoCommand = undoStack.Pop();
+            redoStack.Push(undoCommand);
+            undoCommand.UnExecute.Execute(null);
+            if (Figures.Count != 0)
+                Figures.Move(0, 0); // simulation of a collection change 
+        }
+    }
+
+    private void redoFunc()
+    {
+        if (redoStack.Count > 0)
+        {
+            var redoCommand = redoStack.Pop();
+            undoStack.Push(redoCommand);
+            redoCommand.Execute.Execute(null);
+            if (Figures.Count != 0)
+                Figures.Move(0, 0); // simulation of a collection change 
+        }
+    }
+
+    public RelayCommand Undo
+    {
+        get => new RelayCommand( _ => undoFunc());
+        set => OnPropertyChanged();
+    }
+
+    public RelayCommand Redo
+    {
+        get => new RelayCommand( _ => redoFunc());
+        set => OnPropertyChanged();
+    }
+
+
+    public RelayCommand DrawRectangle
+    {
+        get
+        {
+            return new RelayCommand( _ =>
+            {
+                redoStack.Clear();
+                Random r = new Random();
+                int lwidth = r.Next(0, 500);
+                int lheight = r.Next(0, 500);
+                Point a = new Point(lwidth, lheight);
+                Point b = new Point(lwidth + 20, lheight + 20);
+                Rectangle rectangle = new Rectangle(a, b);
+                Figure figure = new Figure(rectangle, null, null);
+                Figures.Add(figure);
+                undoStack.Push(new UserCommands( _ => { figure.CreateFigure(); }, _ => { figure.DeleteFigure(); }));
+            });
+        }
+    }
+
+    public RelayCommand DoRotate
+    {
+        get
+        {
+            return new RelayCommand( _ =>
+            {
+                redoStack.Clear();
+                Figure figure = Figures[Figures.Count - 1];
+                figure.ModifyFigure(Figure.FigureAction.ROTATE, 30);
+                Figures[Figures.Count - 1] = figure;
+                undoStack.Push(new UserCommands( _ => { figure.ModifyFigure(Figure.FigureAction.ROTATE, 30); }, 
+                                                 _ => { figure.ModifyFigure(Figure.FigureAction.ROTATE, -30); }));
+            });
+        }
+    }
+
+    public RelayCommand DoScale
+    {
+        get
+        {
+            return new RelayCommand( _ =>
+            {
+                redoStack.Clear();
+                Figure figure = Figures[Figures.Count - 1];
+                figure.ModifyFigure(Figure.FigureAction.SCALE, new Point(2,2));
+                Figures[Figures.Count - 1] = figure;
+                undoStack.Push(new UserCommands( _ => { figure.ModifyFigure(Figure.FigureAction.SCALE, new Point(2, 2)); },
+                                                 _ => { figure.ModifyFigure(Figure.FigureAction.SCALE, new Point(0.5, 0.5)); }));
+            });
+        }
+    }
+
+    public RelayCommand DoMove
+    {
+        get
+        {
+            return new RelayCommand( _ =>
+            {
+                redoStack.Clear();
+                Figure figure = Figures[Figures.Count - 1];
+                Random r = new Random();
+                double x =  r.Next(-10, 11);
+                double y = r.Next(-10, 11);
+                Vector v = new Vector(x, y);
+                figure.ModifyFigure(Figure.FigureAction.MOVE, v);
+                Figures[Figures.Count - 1] = figure;
+                undoStack.Push(new UserCommands( _ => { figure.ModifyFigure(Figure.FigureAction.MOVE, v); },
+                                                 _ => { figure.ModifyFigure(Figure.FigureAction.MOVE, -v); }));
+
+            });
+        }
+    }
+
+     public RelayCommand DeleteFigure
+    {
+        get
+        {
+            return new RelayCommand( _ =>
+            {
+                redoStack.Clear();
+                Figure figure = Figures[Figures.Count - 1];
+                figure.DeleteFigure();
+                Figures[Figures.Count - 1] = figure;
+                undoStack.Push(new UserCommands( _ => { figure.DeleteFigure(); }, _ => { figure.CreateFigure(); }));
+            });
+        }
+    }
+
+    public MainWindowVM()
+    {
+        Figures = new ObservableCollection<Figure>();
+        redoStack = new Stack<UserCommands>();
+        undoStack = new Stack<UserCommands>();
+
+    }
     public event PropertyChangedEventHandler PropertyChanged;
 
     [NotifyPropertyChangedInvocator]
--- flop.net/ViewModel/Models/Figure.cs
+++ flop.net/ViewModel/Models/Figure.cs
@@ -2,21 +2,39 @@
 using System.Runtime.CompilerServices;
 using System.Windows.Media;
 using flop.net.Model;
-
+using System.Windows;
+using System.Diagnostics;
 namespace flop.net.ViewModel.Models
 {
     public class Figure : INotifyPropertyChanged
     {
         public event PropertyChangedEventHandler PropertyChanged;
         
-        private PointCollection Points { get; }
-        private IGeometric Geometric { get; }
-        private DrawingParameters DrawingParameters { get; }
+        public enum FigureAction { MOVE, ROTATE, SCALE };
+
+        public PointCollection Points { get; set; }
+        private IGeometric geometric;
+        public IGeometric Geometric 
+        {
+            get
+            {
+                return geometric;
+            }
+            set
+            {
+                geometric = value;
+                OnPropertyChanged();
+            }
+        }
+        public DrawingParameters DrawingParameters { get; set; }
 
         public Figure()
         {
         }
-        
+        static Figure()
+        {
+            EmptyGeometric = new Rectangle(new System.Windows.Point(-1, -1), new System.Windows.Point(-1, -1));
+        }
         public Figure(IGeometric geometric, DrawingParameters drawingParameters, PointCollection points)
         {
             Points = points;
@@ -27,6 +45,42 @@ namespace flop.net.ViewModel.Models
         protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = "")
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private static IGeometric EmptyGeometric;
+        private IGeometric SaveGeometric;
+
+        public void CreateFigure()
+        {
+            Geometric = SaveGeometric;
+        }
+
+        public void DeleteFigure()
+        {
+            SaveGeometric = Geometric;
+            Geometric = EmptyGeometric;
+        }
+
+        public void ModifyFigure(FigureAction action, object parameter)
+        {
+            if (action == FigureAction.MOVE)
+            {
+                Vector vector = (Vector)parameter;
+                if (vector != null)
+                    Geometric.Move(vector);
+            }
+            if (action == FigureAction.ROTATE)
+            {
+                double angle;
+                double.TryParse(parameter.ToString(), out angle);
+                Geometric.Rotate(angle);
+            }
+            if (action == FigureAction.SCALE)
+            {
+                Point p = (Point)parameter;
+                if (p != null)
+                    Geometric.Scale(p);
+            }
         }
     }
 }
