d99a018eda3e495760466db0e5f24a5f44cd08c3
krtisheva <Ksyushartisheva@mail.ru>
2022-03-02 21:54:49 +0700

    Добавлен в фабрику полигонов метод, создающий эллипс

--- flop.net/Model/PolygonBuilder.cs
+++ flop.net/Model/PolygonBuilder.cs
@@ -45,5 +45,20 @@ namespace flop.net.Model
          }
          return new Polygon(points, true);
       }
+
+      public static Polygon CreateEllipse(Point pointA, Point pointB, double amount)
+      {
+         PointCollection points = new PointCollection() { };
+         Point center = new Point((pointA.X + pointB.X) / 2, (pointA.Y + pointB.Y) / 2);
+         double h = Math.Abs(pointA.Y) + Math.Abs(pointB.Y);
+         double w = Math.Abs(pointA.X) + Math.Abs(pointB.X);
+         for (var i = 0; i < amount; i ++)
+         {
+            double x = Math.Cos(2 * Math.PI * i / amount) * w / 2 + center.X;
+            double y = Math.Sin(2 * Math.PI * i / amount) * h / 2 + center.Y;
+            points.Add(new Point(x, y));
+         }
+         return new Polygon(points, true);
+      }
    }
 }
