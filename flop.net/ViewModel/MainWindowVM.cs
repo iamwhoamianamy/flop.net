0fef540acf441ab2dd0b1e546f1ab31f0dc71e1d
Ilya Khechnev <Ilya_Khechnev@outlook.com>
2022-03-11 22:26:57 +0700

    Revert "Delete extra usings"
    This reverts commit 0d71c6b314a28d95e4c8d4d850f0c297e45cb64c.

--- flop.net/ViewModel/MainWindowVM.cs
+++ flop.net/ViewModel/MainWindowVM.cs
@@ -9,6 +9,27 @@ using flop.net.Model;
 
 namespace flop.net.ViewModel;
 
+public class UserCommands
+{
+   private Action<object> execute;
+   private Action<object> unexecute;
+   public UserCommands(Action<object> execute, Action<object> unexecute)
+   {
+      this.execute = execute;
+      this.unexecute = unexecute;
+   }
+
+   public RelayCommand Execute
+   {
+      get => new RelayCommand(execute);
+   }
+
+   public RelayCommand UnExecute
+   {
+      get => new RelayCommand(unexecute);
+   }
+}
+
 public class MainWindowVM : INotifyPropertyChanged
 {
    // public ObservableCollection<Figure> Figures { get; set; }
