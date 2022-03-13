dcad21daf1da94afb77df028772f2c038b24022b
Ilya Khechnev <Ilya_Khechnev@outlook.com>
2022-03-13 14:57:04 +0700

    Fix merge

--- flop.net/ViewModel/MainWindowVM.cs
+++ flop.net/ViewModel/MainWindowVM.cs
@@ -2,6 +2,7 @@
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Collections.Generic;
+using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Windows;
 using flop.net.Annotations;
