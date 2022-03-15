429eabcbcf48391b961d8abf873f66c7b3e4a416
Alexander Izotov <75754617+krokodiller@users.noreply.github.com>
2022-03-15 12:37:22 +0700

    Переделан класс JsonSaver для сериализации контейнера со слоями.

--- flop.net/Save/JsonSaver.cs
+++ flop.net/Save/JsonSaver.cs
@@ -4,33 +4,59 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using System.IO.Compression;
+using System.Collections.ObjectModel;
 using Newtonsoft.Json;
 
 using flop.net.Model;
 
 namespace flop.net.Save
 {
-   public static class JsonSaver
+   public class JsonSaver
    {
-      public static string filePath { get; set; }
-      public static string fileName { get; set; }
-
-      public static void SaveGeometricToJson(IGeometric geometric)
+      public JsonSaver(string filePath, string fileName)
       {
-         Directory.CreateDirectory(filePath);
+         FilePath = filePath;
+         FileName = fileName;
+      }
+      public string FilePath { get; private set; }
+      public string FileName { get; private set; }
+
+      public void SaveLayersToJson(Collection<Layer> layers)
+      {
+         Directory.CreateDirectory(FilePath);
          var settings = new JsonSerializerSettings();
-         settings.TypeNameHandling = TypeNameHandling.Objects;
-         string jsonString = JsonConvert.SerializeObject(geometric, Formatting.Indented, settings);
-         File.WriteAllText(filePath + fileName, jsonString);
+         settings.TypeNameHandling = TypeNameHandling.All;
+         settings.Formatting = Formatting.Indented;
+         string jsonString = JsonConvert.SerializeObject(layers, settings);
+         File.WriteAllText(FilePath + FileName + ".json", jsonString);
       }
 
-      public static IGeometric RestoreGeometricFromJson()
+      public ObservableCollection<Layer> RestoreLayersFromJson()
       {
-         string jsonString = File.ReadAllText(filePath + fileName);
+         string jsonString = File.ReadAllText(FilePath + FileName + ".json");
          var settings = new JsonSerializerSettings();
          settings.TypeNameHandling = TypeNameHandling.Objects;
-         IGeometric geometric = JsonConvert.DeserializeObject<IGeometric>(jsonString, settings);
-         return geometric;
-      }  
+         ObservableCollection<Layer> layers = JsonConvert.DeserializeObject<ObservableCollection<Layer>>(jsonString, settings);
+         return layers;
+      }
+
+      public void CompressJson()
+      {
+         using (var zip = ZipFile.Open(FilePath + FileName + ".zip", ZipArchiveMode.Create))
+         {
+            zip.CreateEntryFromFile(FilePath + FileName + ".json", FileName + ".json");
+         }
+         File.Delete(FilePath + FileName + ".json");
+      }
+
+      public void DecompressJson()
+      {
+         if (File.Exists(FilePath + FileName + ".zip"))
+         {
+            ZipFile.ExtractToDirectory(FilePath + FileName + ".zip", FilePath);
+            File.Delete(FilePath + FileName + ".zip");
+         }
+      }
    }
 }
