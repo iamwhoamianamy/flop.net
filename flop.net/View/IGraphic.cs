d24b1981039e34d6148b638595dd06f5054d28bb
murkykiska <catmasya97@gmail.com>
2022-03-20 19:20:23 +0700

    Начало координат теперь лежит в левом нижнем углу. Добавлена отрисовка BoundaryBox, так же добавлены расчеты scale, scalePoint

--- flop.net/View/IGraphic.cs
+++ flop.net/View/IGraphic.cs
@@ -13,68 +13,68 @@ using Polygon = System.Windows.Shapes.Polygon;
 
 namespace flop.net.View
 {
-    public interface IGraphic
-    {
-        void DrawPolyline(PointCollection points, DrawingParameters drawingParametrs);
-        void DrawPolygon(PointCollection points, DrawingParameters drawingParametrs);
-    }
-    public class Graphic : IGraphic
-    {
-        Canvas canvas;
-        public Graphic(Canvas canvas)
-        {
-            this.canvas = canvas;
-        }
+	public interface IGraphic
+	{
+		void DrawPolyline(PointCollection points, DrawingParameters drawingParametrs);
+		void DrawPolygon(PointCollection points, DrawingParameters drawingParametrs);
+	}
+	public class Graphic : IGraphic
+	{
+		Canvas canvas;
+		public Graphic(Canvas canvas)
+		{
+			this.canvas = canvas;
+		}
 
-        public void CleanCanvas()
-        {
-            canvas.Children.Clear();
-        }
+		public void CleanCanvas()
+		{
+			canvas.Children.Clear();
+		}
 
-        public void DrawPolygon(PointCollection points, DrawingParameters drawingParametrs)
-        {
-            Polygon polygon = new Polygon();
-          
-            polygon.Fill = new SolidColorBrush(drawingParametrs.Fill);
-            polygon.Stroke = new SolidColorBrush(drawingParametrs.Stroke);
-            polygon.StrokeThickness = drawingParametrs.StrokeThickness;
-            polygon.StrokeDashCap = drawingParametrs.PenLineCap;
-            polygon.Opacity = drawingParametrs.Opacity;
+		public void DrawPolygon(PointCollection points, DrawingParameters drawingParametrs)
+		{
+			Polygon polygon = new Polygon();
+		  
+			polygon.Fill = new SolidColorBrush(drawingParametrs.Fill);
+			polygon.Stroke = new SolidColorBrush(drawingParametrs.Stroke);
+			polygon.StrokeThickness = drawingParametrs.StrokeThickness;
+			polygon.StrokeDashCap = drawingParametrs.PenLineCap;
+			polygon.Opacity = drawingParametrs.Opacity;
 
-         foreach (var x in drawingParametrs.StrokeDashArray)
-         {
-            polygon.StrokeDashArray.Add(x);
-         }
+		   foreach (var x in drawingParametrs.StrokeDashArray)
+		   {
+			   polygon.StrokeDashArray.Add(x);
+		   }
 
-         foreach (Point point in points)
-            {
-                polygon.Points.Add(point);
-            }
+			foreach (Point point in points)
+			{				
+				polygon.Points.Add(point);
+			}
 
-            canvas.Children.Add(polygon);
-        }
+			canvas.Children.Add(polygon);
+		}
 
-        public void DrawPolyline(PointCollection points, DrawingParameters drawingParametrs)
-        {
-            Polyline polyline = new Polyline();
+		public void DrawPolyline(PointCollection points, DrawingParameters drawingParametrs)
+		{
+			Polyline polyline = new Polyline();
 
-            //polyline.Fill = new SolidColorBrush(drawingParametrs.Fill);
-            //polyline.Stroke = new SolidColorBrush(drawingParametrs.Stroke);
-            //polyline.StrokeThickness = drawingParametrs.StrokeThickness;
-            //polyline.StrokeDashCap = drawingParametrs.PenLineCap;
-            //polyline.Opacity = drawingParametrs.Opacity;
+			//polyline.Fill = new SolidColorBrush(drawingParametrs.Fill);
+			//polyline.Stroke = new SolidColorBrush(drawingParametrs.Stroke);
+			//polyline.StrokeThickness = drawingParametrs.StrokeThickness;
+			//polyline.StrokeDashCap = drawingParametrs.PenLineCap;
+			//polyline.Opacity = drawingParametrs.Opacity;
 
-            //foreach (var x in drawingParametrs.StrokeDashArray)
-            //{
-            //    polyline.StrokeDashArray.Add(x);
-            //}
+			//foreach (var x in drawingParametrs.StrokeDashArray)
+			//{
+			//	polyline.StrokeDashArray.Add(x);
+			//}
 
-            foreach (Point point in points)
-            {
-                polyline.Points.Add(point);
-            }
+			foreach (Point point in points)
+			{
+				polyline.Points.Add(point);
+			}
 
-            canvas.Children.Add(polyline);
-        }
-    }
+			canvas.Children.Add(polyline);
+		}
+	}
 }
--- flop.net/View/MainWindow.xaml.cs
+++ flop.net/View/MainWindow.xaml.cs
@@ -21,12 +21,15 @@ using System.Windows.Shapes;
 using flop.net.Model;
 using flop.net.Enums;
 using flop.net.Save;
+using System.Windows.Controls.Primitives;
+using Figure = flop.net.Model.Figure;
+using System.Collections.Specialized;
 
 namespace flop.net
 {
    /// <summary>
    /// Логика взаимодействия для MainWindow.xaml
-   /// </summary>
+   /// </summary>   
    public partial class MainWindow : RibbonWindow, INotifyPropertyChanged
    {
       private MainWindowVM mainWindowVM;
@@ -68,18 +71,17 @@ namespace flop.net
 
          Save.MouseLeftButtonDown += SaveOnMouseLeftButtonDown; 
          DrawAll();
-      }
+      }      
 
       private void SaveOnMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
       {
          MainWindowVM.Save.Execute(new SaveParameters { Format = "svg" , Width = (int)MainCanvas.ActualWidth, Height = (int)MainCanvas.ActualHeight });
       }
 
-      private void Figures_CollectionChanged(object sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
+      private void Figures_CollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
       {
          DrawAll();
       }
-
       public event PropertyChangedEventHandler PropertyChanged;
 
       [NotifyPropertyChangedInvocator]
@@ -106,7 +108,160 @@ namespace flop.net
             }            
          }
       }
+      public void DrawBoundaryBox(Figure figure)
+      {
+         //  0----1----2
+         //  |         |
+         //  3         4
+         //  |         |
+         //  5----6----7
 
+         List<Thumb> thumbs = new List<Thumb>();
+         Thumb TopLeft = new Thumb();
+         Thumb TopCenter = new Thumb();
+         Thumb TopRight = new Thumb();
+         Thumb LeftCenter = new Thumb();
+         Thumb RightCenter = new Thumb();
+         Thumb BotLeft = new Thumb();
+         Thumb BotCenter = new Thumb();
+         Thumb BotRight = new Thumb();
+
+         TopLeft.Margin = new Thickness(
+            figure.Geometric.BoundingBox.TopLeft.X - 10,
+            figure.Geometric.BoundingBox.TopLeft.Y - 10,
+            -figure.Geometric.BoundingBox.TopLeft.X + 10,
+            -figure.Geometric.BoundingBox.TopLeft.Y + 10
+            );
+         TopCenter.Margin = new Thickness(
+            figure.Geometric.BoundingBox.TopCenter.X - 5,
+            figure.Geometric.BoundingBox.TopCenter.Y - 10,
+            -figure.Geometric.BoundingBox.TopCenter.X + 5,
+            -figure.Geometric.BoundingBox.TopCenter.Y + 10
+            );
+         TopRight.Margin = new Thickness(
+            figure.Geometric.BoundingBox.TopRight.X,
+            figure.Geometric.BoundingBox.TopRight.Y - 10,
+            -figure.Geometric.BoundingBox.TopRight.X,
+            -figure.Geometric.BoundingBox.TopRight.Y + 10
+            );
+         LeftCenter.Margin = new Thickness(
+            figure.Geometric.BoundingBox.LeftCenter.X - 10,
+            figure.Geometric.BoundingBox.LeftCenter.Y - 5,
+            -figure.Geometric.BoundingBox.LeftCenter.X + 10,
+            -figure.Geometric.BoundingBox.LeftCenter.Y + 5
+            );
+         RightCenter.Margin = new Thickness(
+            figure.Geometric.BoundingBox.RightCenter.X,
+            figure.Geometric.BoundingBox.RightCenter.Y - 5,
+            -figure.Geometric.BoundingBox.RightCenter.X,
+            -figure.Geometric.BoundingBox.RightCenter.Y + 5
+            );
+         BotLeft.Margin = new Thickness(
+            figure.Geometric.BoundingBox.BotLeft.X - 10,
+            figure.Geometric.BoundingBox.BotLeft.Y,
+            -figure.Geometric.BoundingBox.BotLeft.X + 10,
+            -figure.Geometric.BoundingBox.BotLeft.Y
+            );
+         BotCenter.Margin = new Thickness(
+            figure.Geometric.BoundingBox.BotCenter.X - 5,
+            figure.Geometric.BoundingBox.BotCenter.Y,
+            -figure.Geometric.BoundingBox.BotCenter.X + 5,
+            -figure.Geometric.BoundingBox.BotCenter.Y
+            );
+         BotRight.Margin = new Thickness(
+           figure.Geometric.BoundingBox.BotRight.X,
+           figure.Geometric.BoundingBox.BotRight.Y,
+           -figure.Geometric.BoundingBox.BotRight.X,
+           -figure.Geometric.BoundingBox.BotRight.Y
+           );
+
+         TopLeft.Cursor = Cursors.SizeNESW;
+         TopCenter.Cursor = Cursors.SizeNS;
+         TopRight.Cursor = Cursors.SizeNWSE;
+         LeftCenter.Cursor = Cursors.SizeWE;
+         RightCenter.Cursor = Cursors.SizeWE;
+         BotLeft.Cursor = Cursors.SizeNWSE;
+         BotCenter.Cursor = Cursors.SizeNS;
+         BotRight.Cursor = Cursors.SizeNESW;
+
+         TopLeft.VerticalAlignment = VerticalAlignment.Top;
+         TopCenter.VerticalAlignment = VerticalAlignment.Top;
+         TopRight.VerticalAlignment = VerticalAlignment.Top;
+         LeftCenter.VerticalAlignment = VerticalAlignment.Center;
+         RightCenter.VerticalAlignment = VerticalAlignment.Center;
+         BotLeft.VerticalAlignment = VerticalAlignment.Bottom;
+         BotCenter.VerticalAlignment = VerticalAlignment.Bottom;
+         BotRight.VerticalAlignment = VerticalAlignment.Bottom;
+
+         TopLeft.HorizontalAlignment = HorizontalAlignment.Left;
+         TopCenter.HorizontalAlignment = HorizontalAlignment.Center;
+         TopRight.HorizontalAlignment = HorizontalAlignment.Right;
+         LeftCenter.HorizontalAlignment = HorizontalAlignment.Left;
+         RightCenter.HorizontalAlignment = HorizontalAlignment.Right;
+         BotLeft.HorizontalAlignment = HorizontalAlignment.Left;
+         BotCenter.HorizontalAlignment = HorizontalAlignment.Center;
+         BotRight.HorizontalAlignment = HorizontalAlignment.Right;
+
+         thumbs.Add(TopLeft);
+         thumbs.Add(TopCenter);
+         thumbs.Add(TopRight);
+         thumbs.Add(LeftCenter);
+         thumbs.Add(RightCenter);
+         thumbs.Add(BotLeft);
+         thumbs.Add(BotCenter);
+         thumbs.Add(BotRight);
+
+         foreach (var thumb in thumbs)
+         {
+            thumb.Width = 10;
+            thumb.Height = 10;
+            thumb.DragDelta += new DragDeltaEventHandler(ResizeThumb_DragDelta);
+            thumb.Background = Brushes.LightGreen;
+            thumb.BorderBrush = Brushes.DarkGreen;
+
+            MainCanvas.Children.Add(thumb);
+         }
+      }
+      private void ResizeThumb_DragDelta(object sender, DragDeltaEventArgs e)
+      {
+         Point delta = new Point(e.HorizontalChange, e.VerticalChange);
+         Point scalePoint = new Point(0, 0);
+
+         var box = MainWindowVM.ActiveFigure.Geometric.BoundingBox;
+
+         Point size = (Point)(box.TopLeft - box.BotRight);         
+         var thumb = e.OriginalSource as Thumb;
+
+         Point scale = new Point((size.X + delta.X) / size.X, (size.Y + delta.Y) / size.Y);
+
+         switch (thumb.VerticalContentAlignment)
+         {
+            case VerticalAlignment.Top:
+               scalePoint.Y = box.BotCenter.Y;
+               break;
+            case VerticalAlignment.Center:
+               scale.X = 0;
+               break;
+            case VerticalAlignment.Bottom:
+               scalePoint.Y = box.TopCenter.Y;
+               break;
+         }
+
+         switch (thumb.HorizontalContentAlignment)
+         {
+            case HorizontalAlignment.Left:
+               scalePoint.X = box.RightCenter.X;
+               break;
+            case HorizontalAlignment.Center:
+               scale.Y = 0;
+               break;
+            case HorizontalAlignment.Right:
+               scalePoint.Y = box.LeftCenter.Y;
+               break;
+         }
+
+         MainWindowVM.ActiveFigure.Geometric.Scale(scale, scalePoint);
+      }
       private void OnPreviewMouseDown(object sender, MouseButtonEventArgs e)
       {
          MousePosition1 = e.GetPosition(MainCanvas);
@@ -127,7 +282,6 @@ namespace flop.net
             MousePosition2 = e.GetPosition(MainCanvas);
          }
       }
-
       private void OnPreviewMouseMove(object sender, MouseEventArgs e)
       {
          if (mainWindowVM.WorkingMode == ViewMode.Creation &&
@@ -167,12 +321,10 @@ namespace flop.net
             OnPreviewMouseUp(sender, null);
          }
       }
-
       private void OnPreviewMouseUp(object sender, MouseButtonEventArgs e)
       {
          // Место для рекламы ваших заканчивающих команд
       }
-
       private void OnPreviewMouseRightButtonDown(object sender, MouseButtonEventArgs e)
       {
          mainWindowVM.SetActiveFigure.Execute(e.GetPosition(MainCanvas));
