dd66dc9c00c844f7e7a9accf0aab675b36c8fa16
Parfentia <68896490+Parfentia@users.noreply.github.com>
2022-03-21 00:33:39 +0700

    Проверить повороты фигур (#138)
    * Added check assembly and publication application
    
    * Delete build.yml
    
    * Fix download artifacts
    
     Now testers can download builds
    
    * Create AUTHORS.md
    
    * Create .mailmap
    
    * Add authors and Add my mail
    
    * Squash
    
    Update .mailmap
    
    Update .mailmap
    
    * Update .mailmap
    
    * Update .mailmap
    
    * Update issue templates
    
    * Сделала тесты на поворот фигуры
    
    Co-authored-by: ilusha <Ilya_Khechnev@outlook.com>
    Co-authored-by: Arthur Efremov <anon.yu@mail.ru>
    Co-authored-by: killbut <72091291+killbut@users.noreply.github.com>
    Co-authored-by: Artem <strongmouse5088@gmail.com>

---
+++ flop.net.Tests/Geometry/PolygonTests.cs
@@ -0,0 +1,100 @@
+﻿using flop.net.Model;
+using System;
+using System.Linq;
+using System.Windows;
+using System.Windows.Media;
+using Xunit;
+
+namespace flop.net.Tests.Geometry
+{
+   public class PolygonTest
+   {
+      private const double Eps = 1E-10;
+
+      [Fact]
+      public void RotateRectangleTest()
+      {
+         var pointA = new Point(0, 0);
+         var pointB = new Point(2, 2);
+         var rectangle = PolygonBuilder.CreateRectangle(pointA, pointB);
+
+         rectangle.Rotate(90);
+
+         for (var i = 0; i < rectangle.Points.Count; i++)
+         {
+            if (Math.Abs(rectangle.Points[i].X) < Eps)
+               rectangle.Points[i] = new Point(0, rectangle.Points[i].Y);
+            if (Math.Abs(rectangle.Points[i].Y) < Eps)
+               rectangle.Points[i] = new Point(rectangle.Points[i].X, 0);
+         }
+
+         var points = new PointCollection()
+         {
+            new Point(2, 0),
+            new Point(0, 0),
+            new Point(0, 2),
+            new Point(2, 2)
+         };
+
+         Assert.True(points.SequenceEqual(rectangle.Points));
+      }
+
+      [Fact]
+      public void RotateTriangleTest()
+      {
+         var pointA = new Point(0, 0);
+         var pointB = new Point(3, 0);
+         var pointC = new Point(0, 3);
+         var triangle = PolygonBuilder.CreateTriangle(pointA, pointB, pointC);
+
+         triangle.Rotate(90);
+
+         for (var i = 0; i < triangle.Points.Count; i++)
+         {
+            if (Math.Abs(triangle.Points[i].X) < Eps)
+               triangle.Points[i] = new Point(0, triangle.Points[i].Y);
+            if (Math.Abs(triangle.Points[i].Y) < Eps)
+               triangle.Points[i] = new Point(triangle.Points[i].X, 0);
+         }
+
+         var points = new PointCollection()
+         {
+            new Point(2, 0),
+            new Point(2, 3),
+            new Point(-1, 0)
+         };
+
+         Assert.True(points.SequenceEqual(triangle.Points));
+      }
+
+      [Fact]
+      public void RotateEllipseTest()
+      {
+         var pointA = new Point(-2, 1);
+         var pointB = new Point(2, -1);
+         var pointCount = 4;
+         var ellipse = PolygonBuilder.CreateEllipse(pointA, pointB, pointCount);
+
+         ellipse.Rotate(90);
+
+         for (var i = 0; i < pointCount; i++)
+         {
+            if (Math.Abs(ellipse.Points[i].X) < Eps)
+               ellipse.Points[i] = new Point(0, Math.Round(ellipse.Points[i].Y));
+            if (Math.Abs(ellipse.Points[i].Y) < Eps)
+               ellipse.Points[i] = new Point(Math.Round(ellipse.Points[i].X), 0);
+         }
+
+         var points = new PointCollection()
+         {
+            new Point(0, 2),
+            new Point(-1, 0),
+            new Point(0, -2),
+            new Point(1, 0),
+         };
+
+         Assert.True(points.SequenceEqual(ellipse.Points));
+
+      }
+   }
+}
