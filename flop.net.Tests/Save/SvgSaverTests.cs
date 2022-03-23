aa8c9593b659e44e02b044c32ef71a12a741292f
Parfentia <68896490+Parfentia@users.noreply.github.com>
2022-03-24 01:36:48 +0700

    175 сделать тесты для сохранения в pdf и png форматах (#177)
    * Тест для треугольника и эллипса на запись  в файл svg
    
    * Добавила тест на создание pdf файла

--- flop.net.Tests/Save/SvgSaverTests.cs
+++ flop.net.Tests/Save/SvgSaverTests.cs
@@ -1,9 +1,4 @@
 ﻿using flop.net.Save;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using Xunit;
 using System.IO;
@@ -22,14 +17,40 @@ namespace flop.net.Tests.Save
       }
 
       [Fact]
-      public void WriteToSvgFileTest()
+      public void WriteToSvgFileRectangleTest()
       {
          var layer = new Layer();
          var rectangle = PolygonBuilder.CreateRectangle(new Point(100, 100), new Point(250, 250));
          layer.Figures.Add(new Figure(rectangle, new DrawingParameters()));
          var svgSaver = new SvgSaver("test.svg", layer, 1000, 1000);
          svgSaver.Save();
-         var expected = File.ReadAllText("Save/Answers/SvgAnswer.txt");
+         var expected = File.ReadAllText("Save/Answers/SvgAnswerRectangle.txt");
+         var received = File.ReadAllText("test.svg");
+         Assert.Equal(expected, received);
+      }
+
+      [Fact]
+      public void WriteToSvgFileTriangleTest()
+      {
+         var layer = new Layer();
+         var triangle = PolygonBuilder.CreateTriangle(new Point(100, 100), new Point(250, 250));
+         layer.Figures.Add(new Figure(triangle, new DrawingParameters()));
+         var svgSaver = new SvgSaver("test.svg", layer, 1000, 1000);
+         svgSaver.Save();
+         var expected = File.ReadAllText("Save/Answers/SvgAnswerTriangle.txt");
+         var received = File.ReadAllText("test.svg");
+         Assert.Equal(expected, received);
+      }
+
+      [Fact]
+      public void WriteToSvgFileEllipseTest()
+      {
+         var layer = new Layer();
+         var ellipse = PolygonBuilder.CreateEllipse(new Point(-20, 10), new Point(20, -10), 4);
+         layer.Figures.Add(new Figure(ellipse, new DrawingParameters()));
+         var svgSaver = new SvgSaver("test.svg", layer, 1000, 1000);
+         svgSaver.Save();
+         var expected = File.ReadAllText("Save/Answers/SvgAnswerEllipse.txt");
          var received = File.ReadAllText("test.svg");
          Assert.Equal(expected, received);
       }
